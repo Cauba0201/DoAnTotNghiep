aea3061291e7a6920d2124db733d7468f3e113e0
Cauba0201 <91552618+Cauba0201@users.noreply.github.com>
2024-10-11 10:07:09 +0700

    Connected database

---
+++ services/databasepg.js
@@ -0,0 +1,20 @@
+const { Client } = require("pg");
+
+const client = new Client({
+  host: "localhost",
+  user: "postgres",
+  port: 5432,
+  password: "a",
+  database: "postgres",
+});
+
+client.connect().then(() => console.log("connected"))
+
+client.query(`Select * from testping `, (err, res) => {
+  if (!err) {
+    console.log(res.rows);
+  } else {
+    console.log(err.message);
+  }
+  client.end;
+});
