f73811149fa5e4ec8f4ccc0c25d89de84d51a4a2
Cauba0201 <91552618+Cauba0201@users.noreply.github.com>
2024-10-30 14:28:41 +0700

    write delete api with delete method for admin

--- controllers/adminsController.js
+++ controllers/adminsController.js
@@ -1,4 +1,5 @@
 const { PrismaClient } = require("@prisma/client");
+const { param } = require("express-validator");
 const prisma = new PrismaClient();
 
 exports.createAdmins = async (req, res) => {
@@ -55,6 +56,40 @@ exports.updateAdmins = async (req, res) => {
       },
     });
     return res.status(200).json({ updateAdmins });
+  } catch (error) {
+    return res.status(500).json({ error: error.message });
+  }
+};
+
+exports.deleteAdmin = async (req, res) => {
+  try {
+    if (
+      !(await prisma.admins.findUnique({
+        where: { id: parseInt(req.params.id) },
+      }))
+    ) {
+      return res.status(404).json({ error: "Admin not found" });
+    }
+
+    const adminCount = await prisma.admins.count({
+      where: {
+        id: parseInt(req.params.id),
+      },
+    });
+
+    if (adminCount) {
+      return res
+        .status(409)
+        .json({ error: `Admin id is being used in ${adminCount} admin(s)` });
+    }
+
+    await prisma.admins.delete({
+      where: {
+        id: parseInt(req.param.id),
+      },
+    });
+
+    return res.status(200).send();
   } catch (error) {
     return res.status(500).json({ error: error.message });
   }
