cc04cc9945a36539fd197cc311411afcd36483c1
Cauba0201 <91552618+Cauba0201@users.noreply.github.com>
2024-10-30 14:15:07 +0700

    write api with method put is update

--- controllers/adminsController.js
+++ controllers/adminsController.js
@@ -27,3 +27,35 @@ exports.getAdmins = async (req, res) => {
     return res.status(400).json({ message: error.message });
   }
 };
+
+exports.updateAdmins = async (req, res) => {
+  try {
+    if (
+      !(await prisma.admins.findUnique({
+        where: { id: parseInt(req.params.id) },
+      }))
+    ) {
+      return res.status(404).json({ error: "Admin not found" });
+    }
+    if (!req.body.name) {
+      return res.status(422).json({ error: "Name is requered" });
+    }
+    if (await prisma.admins.findUnique({ where: { name: req.body.name } })) {
+      return res
+        .status(409)
+        .json({ error: `${req.body.name} admin already exitsts` });
+    }
+
+    const updateAdmins = await prisma.admins.update({
+      data: {
+        name: req.body.name,
+      },
+      where: {
+        id: parseInt(req.params.id),
+      },
+    });
+    return res.status(200).json({ updateAdmins });
+  } catch (error) {
+    return res.status(500).json({ error: error.message });
+  }
+};
